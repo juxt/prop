326a264c9667182045d7043aa6cf7b48dd0452b4
Ricky Bedia <ricky@juxt.pro>
2017-08-03 09:57:17 +0100

    Add Description and Url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prop "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure Recursive Neural Network"
+  :url "https://github.com/juxt/prop"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
