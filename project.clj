19fdd805049d8d2b1935644263b54af3318bfe56
Ricky Bedia <ricky@juxt.pro>
2017-08-03 14:41:12 +0100

    Cljc conversion

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject prop "0.1.0"
+(defproject prop "0.1.1-test-cljc"
   :description "A Clojure Recursive Neural Network"
   :url "https://github.com/juxt/prop"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.854"]
                  [net.mikera/core.matrix "0.58.0"]])
